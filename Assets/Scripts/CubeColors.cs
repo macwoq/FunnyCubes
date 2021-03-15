84ec5b94fcf210ec400688bf9df9d2530750cad5
macwoq <57508051+macwoq@users.noreply.github.com>
2021-03-15 19:10:43 +0100

    Done

---
+++ Assets/Scripts/CubeColors.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Random = UnityEngine.Random;
+
+public class CubeColors : MonoBehaviour
+{
+    CubeMovement cubeMovement;
+    
+    //Vector3 startPos = new Vector3(0, 0, 0);
+    [Header("Colors")]
+    [SerializeField] Material cubeMaterial;
+    public Color[] cubeColors;
+    public Color colorW = Color.white;
+    public Color colorB = Color.black;
+
+    [Header("Randomize")]
+    public bool isRandom;
+    private float timer;
+    public float timerInterval = 3f;
+    [Header("Black&White")]
+    public bool smoothBW;
+    public float cubeLerpTimer = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        cubeMovement = FindObjectOfType<CubeMovement>();
+        timer = timerInterval; 
+    }
+
+    public void StartRandom()
+    {
+        smoothBW = false;
+        isRandom = true;
+        timerInterval = timer;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRandom || !isRandom)
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+        timerInterval -= Time.deltaTime;
+        if (!smoothBW && isRandom && timerInterval < 0)
+        {
+            RandomLerp();
+            timerInterval = timer;
+        }
+    }
+
+    private void RandomLerp()
+    {
+        var currentColor = Random.Range(0, cubeColors.Length);
+        cubeMaterial.color = cubeColors[currentColor];
+    }
+
+    public void StartSmooth()
+    {
+        smoothBW = true;
+        isRandom = false;
+        if (smoothBW && !isRandom && !cubeMovement.isActiveAndEnabled)
+        {
+            cubeMaterial.color = colorW;
+            StartCoroutine(LerpCol(colorB, cubeLerpTimer));
+        }
+    }
+
+    IEnumerator LerpCol(Color endValue, float duration)
+    {
+        float time = 0;
+        Color startValeu = cubeMaterial.color;
+        while(time < duration)
+        {
+            cubeMaterial.color = Color.Lerp(startValeu, endValue, time/duration);
+            time += Time.deltaTime;
+            yield return null;
+
+        }
+        cubeMaterial.color = endValue;
+        if (smoothBW && !isRandom)
+            StartCoroutine(LerpBack(colorW, cubeLerpTimer));
+    }
+
+    IEnumerator LerpBack(Color endValue, float duration)
+    {
+        float time = 0;
+        Color startValeu = cubeMaterial.color;
+        while (time < duration)
+        {
+            cubeMaterial.color = Color.Lerp(startValeu, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+
+        }
+        cubeMaterial.color = endValue;
+        if (smoothBW && !isRandom && !cubeMovement.isActiveAndEnabled)
+            StartCoroutine(LerpCol(colorB, cubeLerpTimer));
+    }
+}
---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    CubeColors cubeColors;
+    [SerializeField]MinMax minMax;
+    public float speed = 2;
+    private int direction = 1;
+    public bool xMove, yMove, zMove;
+    float mid;
+    Vector3 startPos = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeColors = FindObjectOfType<CubeColors>();
+        cubeColors.isRandom = false;
+        cubeColors.smoothBW = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zMove)
+        {
+            
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time, minMax.maxZ));
+        }
+        if (yMove)
+        {
+            
+            transform.Translate(transform.position.x, direction * speed * Time.deltaTime, transform.position.z);
+            if (transform.position.y >= minMax.maxY)
+            {
+                direction = -1;
+                transform.Translate(transform.position.x, direction * speed * Time.deltaTime, transform.position.z);
+
+
+            }
+            else if (transform.position.y <= minMax.minY)
+            {
+                transform.Translate(transform.position.x, direction * speed * Time.deltaTime, transform.position.z);
+                direction = 1;                
+            }
+            
+        }
+
+        if (xMove)
+        {
+            Vector3 v = startPos;
+            v.x += minMax.maxX * Mathf.Sin(Time.time * speed);
+            transform.position = v;
+        }
+    }
+
+    public void StartX()
+    {
+        transform.position = startPos;
+        xMove = true;
+        yMove = false;
+        zMove = false;
+    }
+
+    public void StartY()
+    {
+        transform.position = startPos;
+        xMove = false;
+        yMove = true;
+        zMove = false;
+    }
+    public void StartZ()
+    {
+        transform.position = startPos;
+        xMove = false;
+        yMove = false;
+        zMove = true;
+    }
+}
+
+[System.Serializable]
+public class MinMax
+{
+    public float maxX = 5, maxY = 3, maxZ = 6;
+    public float minX = -5, minY = -2, minZ = 0;
+} 
